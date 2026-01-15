ee76406b42dbb96cbfb08269a892027128c0d54c
V Yasmeen <70378893+VYasmeen@users.noreply.github.com>
2026-01-15 23:45:06 +0530

    Add basic CSV data ingestion using Python

---
+++ 01_python_foundations/data_ingestion.py
@@ -0,0 +1,14 @@
+import csv
+
+def read_csv(file_path):
+  records = []
+
+  with open(file_path, mode="r, encoding="utf-8") as file:
+      reader = csv.DictReader(file)
+      for row in reader:
+          records.append(row)
+  return records
+
+if __name__ == "__main__":
+    data=read_csv("data/sample_data.csv")
+    print("Total records:" len(data))
