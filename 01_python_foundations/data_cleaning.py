bb241157e8b5ddc27a7174036507128a2e46e23b
Yasmeen <vyasmeen333@gmail.com>
2026-01-17 12:49:54 +0530

    Update data cleaning | yasmeen

--- 01_python_foundations/data_cleaning.py
+++ 01_python_foundations/data_cleaning.py
@@ -1,1 +1,14 @@
+from data_ingestion import read_csv
 
+def clean_data(records):
+    cleaned_records =[]
+
+    for row in records:
+        if row:
+            cleaned_records.append(row)
+    return cleaned_records
+
+if __name__=="__main__":
+    data=read_csv("data/ecommerce_sales.csv")
+    cleaned=clean_data(data)
+    print("Cleaned records:",len(cleaned))
